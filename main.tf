73d22664d8cf898f3514a1ab11f5430d9b674102
VEBER Arnaud <arnaud@veber.pw>
2019-01-18 17:09:27 +0100

    feat(module): implement bypass

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_user" "user" {
+  count = "${var.bypass == "true" ? 0 : 1}"
+
   name = "${var.name}"
 
   tags {
@@ -7,21 +9,21 @@ resource "aws_iam_user" "user" {
 }
 
 resource "aws_iam_user_login_profile" "login" {
-  count = "${var.with_console_access ? 1 : 0}"
+  count = "${var.bypass == "true" || var.with_console_access == "false" ? 0 : 1}"
 
   user    = "${aws_iam_user.user.name}"
   pgp_key = "${var.encoded_pgp_public_key}"
 }
 
 resource "aws_iam_access_key" "access_key" {
-  count = "${var.with_programmatic_access ? 1 : 0}"
+  count = "${var.bypass == "true" || var.with_programmatic_access == "false" ? 0 : 1}"
 
   user    = "${aws_iam_user.user.name}"
   pgp_key = "${var.encoded_pgp_public_key}"
 }
 
 resource "aws_iam_user_ssh_key" "ssh_key" {
-  count = "${var.ssh_key != "" ? 1 : 0}"
+  count = "${var.bypass == "true" || var.ssh_key == "" ? 0 : 1}"
 
   username = "${aws_iam_user.user.name}"
 
