2041c66fcbd70663b809cd13815b60dbe97cede0
VEBER Arnaud <arnaud@veber.pw>
2019-08-27 12:52:42 +0200

    fix(tf): access attributes on specific instance

--- main.tf
+++ main.tf
@@ -11,21 +11,21 @@ resource "aws_iam_user" "user" {
 resource "aws_iam_user_login_profile" "login" {
   count = var.bypass || ! var.with_console_access ? 0 : 1
 
-  user    = aws_iam_user[0].user.name
+  user    = aws_iam_user.user[0].name
   pgp_key = var.encoded_pgp_public_key
 }
 
 resource "aws_iam_access_key" "access_key" {
   count = var.bypass || ! var.with_programmatic_access ? 0 : 1
 
-  user    = aws_iam_user[0].user.name
+  user    = aws_iam_user.user[0].name
   pgp_key = var.encoded_pgp_public_key
 }
 
 resource "aws_iam_user_ssh_key" "ssh_key" {
   count = var.bypass || var.ssh_key == "" ? 0 : 1
 
-  username = aws_iam_user[0].user.name
+  username = aws_iam_user.user[0].name
 
   encoding   = "SSH"
   public_key = var.ssh_key
