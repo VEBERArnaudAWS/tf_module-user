29f9d66c4efe04c609597003bae8440d41419bb4
VEBER Arnaud <arnaud@veber.pw>
2019-08-27 12:20:21 +0200

    refactor(tf): update ~> 0.12

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 resource "aws_iam_user" "user" {
-  count = "${var.bypass == "true" ? 0 : 1}"
+  count = var.bypass == "true" ? 0 : 1
 
-  name = "${var.name}"
+  name = var.name
 
-  tags {
-    terraformed = "true"
+  tags = {
+    terraformed = true
   }
 }
 
 resource "aws_iam_user_login_profile" "login" {
-  count = "${var.bypass == "true" || var.with_console_access == "false" ? 0 : 1}"
+  count = var.bypass == true || var.with_console_access == false ? 0 : 1
 
-  user    = "${aws_iam_user.user.name}"
-  pgp_key = "${var.encoded_pgp_public_key}"
+  user    = aws_iam_user.user.name
+  pgp_key = var.encoded_pgp_public_key
 }
 
 resource "aws_iam_access_key" "access_key" {
-  count = "${var.bypass == "true" || var.with_programmatic_access == "false" ? 0 : 1}"
+  count = var.bypass == true || var.with_programmatic_access == false ? 0 : 1
 
-  user    = "${aws_iam_user.user.name}"
-  pgp_key = "${var.encoded_pgp_public_key}"
+  user    = aws_iam_user.user.name
+  pgp_key = var.encoded_pgp_public_key
 }
 
 resource "aws_iam_user_ssh_key" "ssh_key" {
-  count = "${var.bypass == "true" || var.ssh_key == "" ? 0 : 1}"
+  count = var.bypass == true || var.ssh_key == "" ? 0 : 1
 
-  username = "${aws_iam_user.user.name}"
+  username = aws_iam_user.user.name
 
   encoding   = "SSH"
-  public_key = "${var.ssh_key}"
+  public_key = var.ssh_key
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "bypass" {
-  type = "string"
+  type = string
 }
 
 variable "name" {
-  type = "string"
+  type = string
 }
 
 variable "encoded_pgp_public_key" {
-  type = "string"
+  type = string
 }
 
 variable "with_console_access" {
-  type = "string"
+  type = string
 
   default = false
 }
 
 variable "with_programmatic_access" {
-  type = "string"
+  type = string
 
   default = false
 }
 
 variable "ssh_key" {
-  type = "string"
+  type = string
 
   default = ""
 }
